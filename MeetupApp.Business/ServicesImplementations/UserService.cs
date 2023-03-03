429bf746efc2b23c4f20edcd99e8b948f345fdfb
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-03-03 18:33:01 +0300

    Add UserService

---
+++ MeetupApp.Business/ServicesImplementations/UserService.cs
@@ -0,0 +1,120 @@
+﻿using AutoMapper;
+using MeetupApp.Core.DataTransferObjects;
+using MeetupApp.Core.ServiceAbstractions;
+using MeetupApp.Data.Abstractions;
+using MeetupApp.DataBase.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace MeetupApp.Business.ServicesImplementations
+{
+    public class UserService : IUserService
+    {
+        private readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(IMapper mapper, IConfiguration configuration, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _configuration = configuration;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<UserDto?> GetUserByIdAsync(Guid id)
+        {
+            var entity = await _unitOfWork.Users.GetByIdAsync(id);
+
+            if (entity == null)
+                throw new ArgumentException("Failed to find record in the database that match the specified id.", nameof(id));
+
+            var dto = _mapper.Map<UserDto>(entity);
+            return dto;
+        }
+
+        public async Task<IEnumerable<UserDto>> GetAllUsers()
+        {
+            return (await _unitOfWork.Users
+                .Get()
+                .ToListAsync())
+                .Select(user => _mapper.Map<UserDto>(user))
+                .ToArray();
+        }
+
+        public async Task<UserDto?> GetUserByEmailAsync(string email)
+        {
+            var user = await _unitOfWork.Users.FindBy(us => us.Email.Equals(email),us => us.Role)
+                .AsNoTracking()
+                .Select(user => _mapper.Map<UserDto>(user))
+                .FirstOrDefaultAsync();
+
+            if (user != null) return user;
+
+            throw new ArgumentException("User with specified email doesn't exist. ", nameof(email));
+        }
+
+        public async Task<UserDto?> GetUserByRefreshTokenAsync(Guid refreshToken)
+        {
+            var token = await _unitOfWork.RefreshToken
+            .Get()
+            .Include(token => token.User)
+            .ThenInclude(user => user.Role)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(token => token.Token.Equals(refreshToken));
+
+            if (token != null) return _mapper.Map<UserDto>(token.User);
+
+            throw new ArgumentException("Could not find a token with the specified model . ", nameof(refreshToken));
+        }
+
+        public async Task<bool> IsUserExists(Guid userId)
+        {
+            return await _unitOfWork.Users.Get().AnyAsync(user => user.Id.Equals(userId));
+        }
+
+        public async Task<bool> IsUserExists(string email)
+        {
+            return await _unitOfWork.Users.Get().AnyAsync(user => user.Email.Equals(email));
+        }
+
+        public async Task<bool> CheckUserPassword(string email, string password)
+        {
+            var dbPasswordHash = (await _unitOfWork.Users
+                .Get()
+                .FirstOrDefaultAsync(user => user.Email.Equals(email)))
+                ?.PasswordHash;
+
+            return dbPasswordHash != null && CreateMd5($"{password}.{_configuration["Secret:PasswordSalt"]}").Equals(dbPasswordHash);
+        }
+
+        public async Task<bool> CheckUserPassword(Guid userId, string password)
+        {
+            var dbPasswordHash = (await _unitOfWork.Users.GetByIdAsync(userId))?.PasswordHash;
+
+            return dbPasswordHash != null && CreateMd5($"{password}.{_configuration["Secret:PasswordSalt"]}").Equals(dbPasswordHash);
+        }
+
+        public async Task<int> RegisterUser(UserDto dto, string password)
+        {
+            var user = _mapper.Map<User>(dto);
+
+            user.PasswordHash = CreateMd5($"{password}.{_configuration["Secret:PasswordSalt"]}");
+
+            await _unitOfWork.Users.AddAsync(user);
+            return await _unitOfWork.Commit();
+        }
+
+        private string CreateMd5(string password)
+        {
+            var passwordSalt = _configuration["UserSecrets:PasswordSalt"];
+
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                var inputByte = System.Text.Encoding.UTF8.GetBytes(password + passwordSalt);
+                var hashByte = md5.ComputeHash(inputByte);
+
+                return Convert.ToHexString(hashByte);
+            }
+        }
+    }
+}
