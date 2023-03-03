bffe81da463e84636c085417d75ede63a613313e
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-03-03 17:53:18 +0300

    Some FIx in Program.cs

--- MeetupApp.WebAPI/Program.cs
+++ MeetupApp.WebAPI/Program.cs
@@ -33,12 +33,9 @@ namespace MeetupApp.WebAPI
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
