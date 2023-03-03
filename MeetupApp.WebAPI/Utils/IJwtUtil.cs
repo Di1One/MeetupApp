3b9a319ebd06a5604ff2c34303e05aaefb734af8
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-03-03 20:23:32 +0300

    Add Jwt Token utils

---
+++ MeetupApp.WebAPI/Utils/IJwtUtil.cs
@@ -0,0 +1,11 @@
+﻿using MeetupApp.Core.DataTransferObjects;
+using MeetupApp.WebAPI.Models.Responces;
+
+namespace MeetupApp.WebAPI.Utils
+{
+    public interface IJwtUtil
+    {
+        Task<TokenResponseModel> GenerateTokenAsync(UserDto dto);
+        Task RemoveRefreshTokenAsync(Guid requestRefreshToken);
+    }
+}
