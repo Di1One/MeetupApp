50db58ac7353946cb33a223f4f76ea66a67ec138
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-03-03 17:25:09 +0300

    Add UserProfile for mapping

---
+++ MeetupApp.WebAPI/MappingProfiles/UserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MeetupApp.Core.DataTransferObjects;
+using MeetupApp.DataBase.Entities;
+
+namespace MeetupApp.WebAPI.MappingProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile() 
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(dto => dto.RoleName, opt => opt.MapFrom(entity => entity.Role.Name));
+
+            CreateMap<UserDto, User>()
+                .ForMember(ent => ent.Id, opt => opt.MapFrom(dto => Guid.NewGuid()))
+                .ForMember(ent => ent.RegistrationDate, opt => opt.MapFrom(dto => DateTime.Now));
+        }
+    }
+}
