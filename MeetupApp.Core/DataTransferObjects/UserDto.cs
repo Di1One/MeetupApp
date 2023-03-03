0f2d069ffb5662977667e630c33c5439201ea50c
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-03-03 17:19:20 +0300

    Add UserDto and PatchModel

---
+++ MeetupApp.Core/DataTransferObjects/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MeetupApp.Core.DataTransferObjects
+{
+    public class UserDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public Guid RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
