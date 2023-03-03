e302c7ad3df6b0af66e65feacf66d0c40f5b27d4
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-03-03 22:52:16 +0300

    EventRpofile for AutoMapper

---
+++ MeetupApp.WebAPI/MappingProfiles/EventProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace MeetupApp.WebAPI.MappingProfiles
+{
+    public class EventProfile : Profile
+    {
+        public EventProfile()
+        {
+            // todo add rules for EventProfile 
+        }
+    }
+}
