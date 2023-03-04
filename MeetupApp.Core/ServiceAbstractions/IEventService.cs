53f0509af355b19276e618a1575b2bcf0970324c
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-03-04 12:48:29 +0300

    Add Event Service

--- MeetupApp.Core/ServiceAbstractions/IEventService.cs
+++ MeetupApp.Core/ServiceAbstractions/IEventService.cs
@@ -4,11 +4,11 @@ namespace MeetupApp.Core.ServiceAbstractions
 {
     public interface IEventService
     {
-        Task<EventDto> GetByIdAsync(Guid id);
+        Task<EventDto> GetEventByIdAsync(Guid id);
         Task<List<EventDto>> GetEventsAsync();
         Task<int> CreateEventAsync(EventDto dto);
         Task<int> UpdateAsync(EventDto dto);
-        Task<int> PatcheEventAsync(Guid id, List<PatchModel> patchList);
+        Task<int> PatchEventAsync(Guid id, List<PatchModel> patchList);
         Task<int> DeleteEventAsync(EventDto dto);
     }
 }
