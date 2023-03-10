e709decf3afc3d419e4b8b3a1fba6f1ff16d0fcd
Kirill Vdovenko <60675403+Di1One@users.noreply.github.com>
2023-03-10 17:45:02 +0300

    Add Service Extensions for Program.cs

---
+++ MeetupApp.WebAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,113 @@
+﻿using MeetupApp.Data.Abstractions.Repositories;
+using MeetupApp.Data.Abstractions;
+using MeetupApp.Data.Repositories;
+using MeetupApp.DataBase.Entities;
+using MeetupApp.Business.ServicesImplementations;
+using MeetupApp.Core.ServiceAbstractions;
+using MeetupApp.WebAPI.Utils;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Serilog;
+using Serilog.Events;
+using System.Reflection;    
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Win32;
+using System.Runtime.CompilerServices;
+using Microsoft.OpenApi.Models;
+
+namespace MeetupApp.WebAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void RegisterLogging(this IHostBuilder host)
+        {
+            host.UseSerilog((ctx, lc) => lc
+              .WriteTo.Console()
+              .WriteTo.File(GetPathToLogFile(),
+                  LogEventLevel.Information));
+        }
+
+        // Returns the path for log file recording.
+        private static string GetPathToLogFile()
+        {
+            var sb = new StringBuilder();
+            sb.Append(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            sb.Append(@"\logs\");
+            sb.Append($"{DateTime.Now:yyyyMMddhhmmss}");
+            sb.Append("data.log");
+            return sb.ToString();
+        }
+
+        public static void RegisterRepositories(this IServiceCollection collection)
+        {
+            collection.AddScoped<IRepository<User>, Repository<User>>();
+            collection.AddScoped<IRepository<Role>, Repository<Role>>();
+            collection.AddScoped<IRepository<RefreshToken>, Repository<RefreshToken>>();
+            collection.AddScoped<IRepository<Event>, Repository<Event>>();
+            collection.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+
+        public static void RegisterBusinessServices(this IServiceCollection collection)
+        {
+            collection.AddScoped<IRoleService, RoleService>();
+            collection.AddScoped<IUserService, UserService>();
+            collection.AddScoped<IRefreshTokenService, RefreshTokenService>();
+            collection.AddScoped<IEventService, EventService>();
+            collection.AddScoped<IJwtUtil, JwtUtil>();
+        }
+
+        public static void RegisterAuth(this IServiceCollection collection, WebApplicationBuilder builder)
+        {
+            collection.AddAuthentication(options => 
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(opt =>
+            {
+                // Only for develop environment.
+                opt.RequireHttpsMetadata = false;
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidIssuer = builder.Configuration["Token:Issuer"],
+                    ValidAudience = builder.Configuration["Token:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:JwtSecret"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+        }
+
+        public static void RegisterSwaggerGenerator(this IServiceCollection collection, WebApplicationBuilder builder)
+        {
+            collection.AddSwaggerGen(options =>
+            {
+                options.IncludeXmlComments(builder.Configuration["XmlDoc"]);
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
+            });
+        }
+    }
+}
--- MeetupApp.WebAPI/Program.cs
+++ MeetupApp.WebAPI/Program.cs
@@ -1,19 +1,6 @@
-using MeetupApp.Business.ServicesImplementations;
-using MeetupApp.Core.ServiceAbstractions;
-using MeetupApp.Data.Abstractions.Repositories;
-using MeetupApp.Data.Abstractions;
 using MeetupApp.DataBase;
-using MeetupApp.DataBase.Entities;
 using Microsoft.EntityFrameworkCore;
-using Serilog;
-using Serilog.Events;
-using System.Reflection;
-using System.Text;
-using MeetupApp.Data.Repositories;
-using MeetupApp.WebAPI.Utils;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+using MeetupApp.WebAPI.Extensions;
 
 namespace MeetupApp.WebAPI
 {
@@ -23,10 +10,7 @@ namespace MeetupApp.WebAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Host.UseSerilog((ctx, lc) => lc
-               .WriteTo.Console()
-               .WriteTo.File(GetPathToLogFile(),
-                   LogEventLevel.Information));
+            builder.Host.RegisterLogging();
 
             // Add services to the container.
 
@@ -36,72 +20,15 @@ namespace MeetupApp.WebAPI
 
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-            // Add business services
-            builder.Services.AddScoped<IRoleService, RoleService>();
-            builder.Services.AddScoped<IUserService, UserService>();
-            builder.Services.AddScoped<IRefreshTokenService, RefreshTokenService>();
-            builder.Services.AddScoped<IEventService, EventService>();
-            builder.Services.AddScoped<IJwtUtil, JwtUtil>();
-
-            // Add repositories
-            builder.Services.AddScoped<IRepository<User>, Repository<User>>();
-            builder.Services.AddScoped<IRepository<Role>, Repository<Role>>();
-            builder.Services.AddScoped<IRepository<RefreshToken>, Repository<RefreshToken>>();
-            builder.Services.AddScoped<IRepository<Event>, Repository<Event>>();
-            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+            builder.Services.RegisterBusinessServices();
+            builder.Services.RegisterRepositories();
 
             builder.Services.AddControllers();
 
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.IncludeXmlComments(builder.Configuration["XmlDoc"]);
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please enter a valid token",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.Http,
-                    BearerFormat = "JWT",
-                    Scheme = "Bearer"
-                });
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type=ReferenceType.SecurityScheme,
-                                Id="Bearer"
-                            }
-                        },
-                        new string[]{}
-                    }
-                });
-            });
+            builder.Services.RegisterSwaggerGenerator(builder);
 
-            builder.Services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(opt =>
-                {
-                    // Only for develop environment.
-                    opt.RequireHttpsMetadata = false;
-                    opt.SaveToken = true;
-                    opt.TokenValidationParameters = new TokenValidationParameters()
-                    {
-                        ValidIssuer = builder.Configuration["Token:Issuer"],
-                        ValidAudience = builder.Configuration["Token:Issuer"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:JwtSecret"])),
-                        ClockSkew = TimeSpan.Zero
-                    };
-                });
+            builder.Services.RegisterAuth(builder);
 
             var app = builder.Build();
 
@@ -119,17 +46,6 @@ namespace MeetupApp.WebAPI
             app.MapControllers();
 
             app.Run();
-        }
-
-        // Returns the path for log file recording.
-        private static string GetPathToLogFile()
-        {
-            var sb = new StringBuilder();
-            sb.Append(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-            sb.Append(@"\logs\");
-            sb.Append($"{DateTime.Now:yyyyMMddhhmmss}");
-            sb.Append("data.log");
-            return sb.ToString();
         }
     }
 }
