a4379ead30569150783851917350fee6f5e8e790
Di1One <kirillwdovenko@mail.ru>
2023-05-06 02:22:37 +0300

    add GlobalExceptionHandler

---
+++ MeetupApp.WebAPI/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+
+namespace MeetupApp.WebAPI.Middlewares
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"{ ex.Message }. { Environment.NewLine } { ex.StackTrace }, An error has occurred");
+
+                if (ex is ArgumentNullException)
+                {
+                    context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                    await context.Response.WriteAsync("An error occurred: argument is null.");
+                }
+                else if (ex is InvalidOperationException)
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    await context.Response.WriteAsync("An error occurred: invalid operation.");
+                }
+                else
+                {
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    await context.Response.WriteAsync("An error has occurred. Please contact support.");
+                }
+            }
+        }
+    }
+}
